862f71048d3c44770cbc89e21dae04b4cab231d8
thaiscardosomello <thais.cardoso.mello@gmail.com>
2024-08-30 22:16:19 -0300

    Exercicio1

---
+++ src/main/java/com/poolista/poolista/Exercicio1.java
@@ -0,0 +1,22 @@
+package com.poolista.poolista;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void resolucao() {
+
+        int n1, n2;
+
+        System.out.print("Digite o primeiro numero: ");
+        Scanner scanner1 = new Scanner(System.in);
+        n1 = scanner1.nextInt(); // leia o próximo inteiro
+
+        System.out.println("Digite o segundo numero: ");
+        n2 = scanner1.nextInt();
+
+        System.out.println("A soma é = "+(n1+n2));
+
+    }
+        
+}
--- src/main/java/com/poolista/poolista/PoolistaApplication.java
+++ src/main/java/com/poolista/poolista/PoolistaApplication.java
@@ -9,25 +9,9 @@ public class PoolistaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PoolistaApplication.class, args);
 
-		import java.util.Scanner;
+		Exercicio1.resolucao();
 
-		public class Exercicio1 {
-			public static void resolucao() {
-
-				int n1, n2;
-
-				System.out.print("Digite o primeiro numero: ");
-				Scanner scanner1 = new Scanner(System.in);
-				n1 = scanner1.nextInt(); // leia o próximo inteiro
-
-				System.out.println("Digite o segundo numero: ");
-				n2 = scanner1.nextInt();
-
-				System.out.println("A soma é = "+(n1+n2));
-
-			}
-
-		}	
+		
 	}
 
 }
