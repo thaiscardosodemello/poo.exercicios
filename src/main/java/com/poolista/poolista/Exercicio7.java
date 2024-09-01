a978bedf40ad4ffb77c4480979cac9d54b09fd67
thaiscardosomello <thais.cardoso.mello@gmail.com>
2024-08-31 22:29:34 -0300

    Exercício 7

---
+++ src/main/java/com/poolista/poolista/Exercicio7.java
@@ -0,0 +1,34 @@
+package com.poolista.poolista;
+
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void resolucao() {
+        //7) Escreva um programa que receba quatro notas de um aluno e calcule a média aritmética dessas notas.
+
+        System.out.println("\n------------------------------------------------------\n");
+        System.out.println("           Calculadora de Média das Notas");
+        System.out.println("\n------------------------------------------------------\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        System.out.print("Digite a quarta nota: ");
+        double nota4 = scanner.nextDouble();
+
+        double media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.printf("\nA média das notas é %.2f.%n", media);
+        System.out.println("\n------------------------------------------------------\n");
+        scanner.close(); 
+    }
+
+}
--- src/main/java/com/poolista/poolista/PoolistaApplication.java
+++ src/main/java/com/poolista/poolista/PoolistaApplication.java
@@ -14,6 +14,7 @@ public class PoolistaApplication {
 		//Exercicio3.resolucao();
 		//Exercicio4.resolucao();
 		//Exercicio5.resolucao();
-		Exercicio6.resolucao();		
+		//Exercicio6.resolucao();		
+		Exercicio7.resolucao();
 	}
 }
