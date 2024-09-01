01c4e5ce88f03797ec198726cf13225f1a3d79b0
thaiscardosomello <thais.cardoso.mello@gmail.com>
2024-08-31 22:20:39 -0300

    Exercicio 6 + Ajuste no Exercício 4

---
+++ src/main/java/com/poolista/poolista/Exercicio6.java
@@ -0,0 +1,31 @@
+package com.poolista.poolista;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void resolucao() {
+        //6) Construa um algoritmo que leia
+        //1- a distância percorrida por um veículo em km
+        //2- o total gasto em combustível em litros.
+        //No final deverá ser respondido o consumo médio deste veículo em km/l.
+
+        System.out.println("\n------------------------------------------------------\n");
+        System.out.println("     Calculadora de Consumo Médio de Combustível");
+        System.out.println("\n------------------------------------------------------\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a distância percorrida pelo veículo em quilômetros (km): ");
+        double distanciaPercorrida = scanner.nextDouble();
+
+        System.out.print("Digite o total gasto em combustível em litros (l): ");
+        double totalGastoCombustivel = scanner.nextDouble();
+
+        double consumoMedio = distanciaPercorrida / totalGastoCombustivel;
+
+        System.out.printf("\nO consumo médio do veículo é %.2f km/l%n", consumoMedio);
+        System.out.println("\n------------------------------------------------------\n");
+        scanner.close(); 
+    }
+
+}
